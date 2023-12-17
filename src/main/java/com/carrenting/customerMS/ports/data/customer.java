ef7cc7e75390d7231efcd3b37d15f79fd50d6d0d
Jan Felix van Son <41387227+janfelixvs@users.noreply.github.com>
2023-12-17 13:50:01 +0100

    customer created

---
+++ src/main/java/com/carrenting/customerMS/ports/data/customer.java
@@ -0,0 +1,15 @@
+package com.carrenting.customerMS.ports.data;
+
+
+
+public class customer {
+
+    private String name;
+    private String firstName;
+    private String address; //Street and house number
+    private String city;
+    private String postalCode;
+
+
+
+}
