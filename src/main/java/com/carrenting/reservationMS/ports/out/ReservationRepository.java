0058582b604321277abc91f3355c77a9604766f7
Jan Felix van Son <41387227+janfelixvs@users.noreply.github.com>
2023-12-26 14:50:57 +0100

    Trying to understand JBA

--- src/main/java/com/carrenting/reservationMS/ports/out/ReservationRepository.java
+++ src/main/java/com/carrenting/reservationMS/ports/out/ReservationRepository.java
@@ -1,10 +1,17 @@
 package com.carrenting.reservationMS.ports.out;
+import com.carrenting.reservationMS.ports.data.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Date;
 
-public interface ReservationRepository extends JpaRepository {
 
-    void addReservation();
-    void deleteReservation();
+
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    void addReservation(Date startDate, Date endDate);
+    void deleteReservation(Long id);
+
+
 
 
 
--- src/main/java/com/carrenting/reservationMS/service/ReservationService.java
+++ src/main/java/com/carrenting/reservationMS/service/ReservationService.java
@@ -2,11 +2,19 @@ package com.carrenting.reservationMS.service;
 
 import com.carrenting.reservationMS.ports.data.Reservation;
 import com.carrenting.reservationMS.ports.in.ReservationManager;
+import com.carrenting.reservationMS.ports.out.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ReservationService implements ReservationManager {
-    @Override
-    public void addReservation(Reservation reservation) {
 
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+
+    @Override
+
+    public void addReservation(Reservation reservation) {
+        reservationRepository.addReservation();
     }
 
     @Override
